159dd0e0de5d1e5b184f65f9e96c9f2ca7c3a78a
Matheus Raposo <matheus_raposo@hotmail.com>
2022-07-07 06:31:34 -0300

    create sample grpc communication in dotnet

---
+++ GrpcGreeter/Program.cs
@@ -0,0 +1,33 @@
+using Grpc.Net.Client;
+using GrpcGreeter.Services;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Additional configuration is required to successfully run gRPC on macOS.
+// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+builder.WebHost.ConfigureKestrel(options =>
+{
+    // Setup a HTTP/2 endpoint without TLS.
+    options.ListenLocalhost(5115, o => o.Protocols =
+        HttpProtocols.Http2);
+});
+
+// This switch must be set before creating the GrpcChannel/HttpClient.
+AppContext.SetSwitch(
+    "System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+
+// The port number(5000) must match the port of the gRPC server.
+var channel = GrpcChannel.ForAddress("http://localhost:5115");
+// var client = new Greet.GreeterClient(channel);
+
+// Add services to the container.
+builder.Services.AddGrpc();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapGrpcService<GreeterService>();
+app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+app.Run();
